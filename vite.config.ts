9840dcc952d8fb5bd78bfec4bc97ab914d372387
Mahbub Rahman <mrahman@econsys.com>
2026-05-29 22:06:58 -0400

    added -- --host option for vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { host: true },
   base: '/reward-catcher/',
   define: {
     'process.env': process.env,
